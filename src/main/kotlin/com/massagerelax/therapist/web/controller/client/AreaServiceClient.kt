c007e762501915561f9c47b90cf5d5ded99d499b
Martin Muller <martin.muller@alfresco.com>
2019-03-23 17:00:05 +0000

    change feign for use area service

--- src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
+++ src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name="area", url="\${area.host}:\${area.port}\${area.context.path}")
+@FeignClient(name="area")
 interface AreaServiceClient {
 
     @GetMapping("api/therapists/long/{long}/lat/{lat}")
