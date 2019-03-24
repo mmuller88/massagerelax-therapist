5766eec98c96d2a10b9214b25720904fc6dccb16
Martin Muller <martin.muller@alfresco.com>
2019-03-24 17:45:27 +0000

    try feign example

--- src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
+++ src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
@@ -3,11 +3,13 @@ package com.massagerelax.therapist.web.controller.client
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 
 @FeignClient(name="area")
+@RequestMapping("/api")
 interface AreaServiceClient {
 
-    @GetMapping("api/therapists/long/{long}/lat/{lat}")
+    @GetMapping("/therapists/long/{long}/lat/{lat}")
     fun retrieveTherapists(@PathVariable("long") long: Double, @PathVariable("lat") lat: Double) : List<String>
 
 }
