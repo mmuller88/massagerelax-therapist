68365a474175ebf1fcc980ecb7fe63e7733fdd98
Martin Muller <martin.muller@alfresco.com>
2019-03-25 22:15:13 +0000

    use rewrite

--- src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
+++ src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
-@FeignClient(name="area")
+@FeignClient(name="area", path = "/area")
 interface AreaServiceClient {
 
     @GetMapping("/therapists/long/{long}/lat/{lat}")
