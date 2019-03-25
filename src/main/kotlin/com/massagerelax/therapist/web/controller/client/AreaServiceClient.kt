a4cf36c86a38b7d88292967a0cf32c9ac1064bc6
Martin Muller <martin.muller@alfresco.com>
2019-03-25 23:16:14 +0000

    use rewrite

--- src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
+++ src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
-@FeignClient(name="area", path = "/area")
+@FeignClient(name="area", path = "/area/api")
 interface AreaServiceClient {
 
     @GetMapping("/therapists/long/{long}/lat/{lat}")
     fun retrieveTherapists(@PathVariable("long") long: Double, @PathVariable("lat") lat: Double) : List<String>
 
-    @GetMapping("/alive")
+    @GetMapping("alive")
     fun checkAlive() : String
 
 }
