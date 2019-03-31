30730564132915c565fb60de7a2bddbc6599c967
Martin Muller <martin.muller@alfresco.com>
2019-03-31 08:56:34 +0100

    add area url to therapist service

--- src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
+++ src/main/kotlin/com/massagerelax/therapist/web/controller/client/AreaServiceClient.kt
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name="area", path = "/api")
+@FeignClient(name="area", path = "/api", url = "\${area.url}")
 interface AreaServiceClient {
 
     @GetMapping("/therapists/long/{long}/lat/{lat}")
